64dbdff4dd908467bd2e34d2e244e8f18c2dcca0
SREERAG O B <21716@fisat>
2018-04-18 01:19:05 -0400

    Modified to add 2 ,3 &4 numbers

--- add.py
+++ add.py
@@ -4,6 +4,10 @@ def add2(num1,num2):
 def add3(num1,num2,num3):
     r=num1+num2+num3
     return r
+def add4(n1,n2,n3,n4):
+    t=n1+n2+n3+n4
+    return t
 
-print add2(10,20)
-print add3(90,10,100)
+print "The sum of two numbers as entered in the function: ", add2(10,20)
+print " The sum of three numbers function is: ",add3(90,10,100)
+print "The sum of four numbers as entered in the function is : ",add4(1,2,3,4)
